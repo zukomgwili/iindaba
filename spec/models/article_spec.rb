c24f9a84e54635c0c5224be9d0d86246755f27fd
Zuko Mgwili <zmgwili@8thlight.com>
2022-11-16 16:46:25 +0000

    refactor: remove redundant code in tests

--- spec/models/article_spec.rb
+++ spec/models/article_spec.rb
@@ -10,15 +10,11 @@ RSpec.describe Article, type: :model do
   it 'should not save article without body' do
     article = Article.new(title: 'This is a title')
 
-    article.save
-
     expect(article.save).to be(false)
   end
 
   it 'should not save article with body less than 10 characters' do
     article = Article.new(title: 'This is a title', body: 'Less than')
-
-    article.save
 
     expect(article.save).to be(false)
   end
