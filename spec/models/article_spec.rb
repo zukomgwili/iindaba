79a2a788ea8a1d8fbc04985ffd4e39ced2ae685f
Zuko Mgwili <zmgwili@8thlight.com>
2022-11-16 16:26:26 +0000

    feat: article should not be saved without title

--- spec/models/article_spec.rb
+++ spec/models/article_spec.rb
@@ -1,5 +1,11 @@
 require 'rails_helper'
 
 RSpec.describe Article, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it 'should not save article without title' do
+    article = Article.new(body: 'This is the body of an article')
+
+    article.save
+
+    expect { article.reload }.to raise_error(ActiveRecord::RecordNotFound)
+  end
 end
